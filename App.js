732eebbfec065158b662285585e11de34d34fced
njj-abs <njj@abstractit.in>
2023-01-30 15:31:07 +0530

    Imported the expo-dev-client.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
+import 'expo-dev-client';
 import React from 'react';
 import { useState } from 'react';
-import { Button, View } from 'react-native';
+import { View } from 'react-native';
 import Main from './src/index';
 
 export default function App() {
